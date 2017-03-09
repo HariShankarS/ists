a8405880520a3e3c58f030fbb90513d9b453a7ea
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2017-03-09 14:53:36 +0530

    Added SideNav

---
+++ side_nav.js
@@ -0,0 +1,8 @@
+ 
+  function openNav() {
+      document.getElementById("sidebar-wrapper").style.width = "200px";
+  }
+
+  function closeNav() {
+      document.getElementById("sidebar-wrapper").style.width = "0";
+  }
